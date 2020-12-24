e57e97b7bfea33030a1e333bb49b2f4276384189
milanmenezes <milanmenezes@gmail.com>
2020-12-24 22:05:23 +0530

    title case

--- js/dynamic.js
+++ js/dynamic.js
@@ -1,11 +1,22 @@
-var messages = ["May this Christmas season bring you closer to all those that you treasure in your heart. Have a Merry Christmas and a Happy New year!"]
+var messages = ["May this Christmas season bring you closer to all those that you treasure in your heart. Have a Merry Christmas and a Happy New year!"];
 document.getElementById("message").innerHTML = messages[0];
 var queryString = _.chain( location.search.slice( 1 ).split( /&/g ) ).map( function ( item ) { if ( item ) return item.split( /=/ ).map( function ( str ) { return decodeURIComponent( str ); } ); } ).compact().object().value();
 console.log(queryString);
+
+var name;
 if(queryString.name){
-  document.getElementById("name").innerHTML=queryString.name+"!";
-  document.title="Merry Christmas "+queryString.name+"!";
-  document.getElementsByTagName('meta')["description"].content = "Merry Christmas "+queryString.name+"!";
+	name=queryString.name;
+}
+if(queryString.n){
+  name=queryString.n;
+}
+
+if(name){
+  name=name.replace("-"," ");
+  name=titleCase(name);
+  document.getElementById("name").innerHTML=name+"!";
+  document.title="Merry Christmas "+name+"!";
+  document.getElementsByTagName('meta')["description"].content = "Merry Christmas "+name+"!";
 }
 if(queryString.from){
   document.getElementById("from").innerHTML=queryString.from;
@@ -14,3 +25,14 @@ if(queryString.from){
 document.addEventListener("click",()=>{    
   document.getElementById('music').play();
   document.removeEventListener('click', this);})
+
+function titleCase(str) {
+   var splitStr = str.toLowerCase().split(' ');
+   for (var i = 0; i < splitStr.length; i++) {
+       // You do not need to check if i is larger than splitStr length, as your for does that for you
+       // Assign it back to the array
+       splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
+   }
+   // Directly return the joined string
+   return splitStr.join(' '); 
+}
